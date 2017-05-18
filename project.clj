e8c1208b2c1ffe509d4a588c819c64173e934254
Toshiki Takeuchi <take@xcoo.jp>
2017-05-18 16:26:38 +0900

    Bump some dependencies version up

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.521" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.542" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.6"]
             [lein-cloverage "1.0.9"]
             [lein-codox "0.10.3"]
             [lein-doo "0.1.7"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha15"]]}}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha16"]]}}
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo/"
                                       :username [:env/clojars_username :gpg]
                                       :password [:env/clojars_password :gpg]}]]
