3e0ce0452c2a232061815ec6f07656c224328336
Toshiki Takeuchi <take@xcoo.jp>
2017-12-02 14:04:32 +0900

    Release 0.2.3

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ clj-hgvs provides:
 With Leiningen/Boot:
 
 ```clojure
-[clj-hgvs "0.2.2"]
+[clj-hgvs "0.2.3"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-hgvs "0.2.3-SNAPSHOT"
+(defproject clj-hgvs "0.2.3"
   :description "Clojure(Script) library for handling HGVS"
   :url "https://github.com/chrovis/clj-hgvs"
   :license {:name "Apache License, Version 2.0"
