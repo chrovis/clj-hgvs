87c1619faffcd136f16b2423bdbcc96df441eb96
Toshiki Takeuchi <take@xcoo.jp>
2018-10-03 13:22:59 +0900

    Set default clojure version to 1.9.0

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/chrovis/clj-hgvs"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.339" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-cloverage "1.0.13"]
             [lein-codox "0.10.4"]
             [lein-doo "0.1.10"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha6"]]}}
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo/"
                                       :username [:env/clojars_username :gpg]
