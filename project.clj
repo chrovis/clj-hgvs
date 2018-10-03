2e69cc242242e6a1bb89b6691b4dd653f28c213e
Toshiki Takeuchi <take@xcoo.jp>
2018-10-03 13:24:01 +0900

    Drop clojure 1.7 support

--- project.clj
+++ project.clj
@@ -9,8 +9,7 @@
             [lein-cloverage "1.0.13"]
             [lein-codox "0.10.4"]
             [lein-doo "0.1.10"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha6"]]}}
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo/"
                                       :username [:env/clojars_username :gpg]
