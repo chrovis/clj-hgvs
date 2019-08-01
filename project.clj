6ed7d88cfffdfb0043495937f25d3ea447ba9bd7
Toshiki Takeuchi <take@xcoo.jp>
2019-08-01 16:59:39 +0900

    Update lein-codox to 0.10.7

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "1.10.439" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-cloverage "1.0.13"]
-            [lein-codox "0.10.5"]
+            [lein-codox "0.10.7"]
             [lein-doo "0.1.11"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [codox-theme-rdash "0.1.2"]]}
