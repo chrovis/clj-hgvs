6c3d0e560710326b8d5a458fd39ec607f5cf9dd0
Toshiki Takeuchi <take@xcoo.jp>
2022-07-12 19:45:34 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.520" :scope "provided"]]
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-cloverage "1.1.2"]
-            [lein-codox "0.10.7"]
+                 [org.clojure/clojurescript "1.11.60" :scope "provided"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [lein-cloverage "1.2.4"]
+            [lein-codox "0.10.8"]
             [lein-doo "0.1.11"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0"]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]
                                   [codox-theme-rdash "0.1.2"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [clojure-future-spec "1.9.0"]]}
